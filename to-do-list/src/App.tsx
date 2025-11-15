d44f13048e30019dfe5fc6da01e6bfaa51bb3924
Nathan Delgado <0nathandelgado0@gmail.com>
2025-11-15 17:01:22 -0300

    Updated Task Interface

--- to-do-list/src/App.tsx
+++ to-do-list/src/App.tsx
@@ -1,28 +1,64 @@
 import "./App.css";
 import Tasks from "./components/Tasks.tsx";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import type { Task } from "./types/types.ts";
 
 function App() {
+  const [tasks, setTasks] = useState<Task[]>([
+    {
+      id: uuidv4(),
+      title: "Estudar React",
+      description: "Estudar React para me tornar um Dev front-end.",
+      isCompleted: false,
+    },
+    {
+      id: uuidv4(),
+      title: "Estudar SQL",
+      description: "Estudar SQL para me tornar um Dev back-end.",
+      isCompleted: false,
+    },
+  ]);
+
+  const [title, setTitle] = useState("")
+
+  const [description, setDescription] = useState("")
+
   return (
     <>
       <div className="flex flex-col bg-linear-to-bl from-purple-950 to-blue-800 min-h-screen">
         <div className="flex flex-col justify-center items-center gap-4 p-4 select-none ">
-          <h1 className="text-white font-bold font-inter text-3xl">Lista de Tarefas</h1>
-          <input
-            type="text"
-            className="border border-white text-white focus:outline-none focus:ring-1 focus:ring-white rounded-md p-2 w-85"
-            placeholder="Digite o título da tarefa"
-          />
-          <input
-            type="text"
-            className="border border-white text-white focus:outline-none focus:ring-1 focus:ring-white rounded-md p-2 w-85"
-            placeholder="Digite a descrição da tarefa"
-          />
-          <button className="border rounded-md cursor-pointer w-85 bg-white font-bold">
-            Adicionar Tarefa
-          </button>
+          <h1 className="text-white font-bold font-[Inter] text-3xl uppercase">
+            Lista de Tarefas
+          </h1>
+          <form className="flex flex-col gap-y-4">
+            <input
+              type="text"
+              value={title}
+              onChange={(e)=>{
+                setTitle(e.target.value)
+              }}
+              className="border border-white text-white text-[14px] focus:outline-none focus:ring-1 focus:ring-white rounded-md p-1 w-85 font-[Inter]"
+              placeholder="Digite o título da tarefa"
+            />
+            <input
+              type="text"
+              value={description}
+              onChange={(e)=>{
+                setDescription(e.target.value)
+              }}
+              className="border border-white text-white text-[14px] focus:outline-none focus:ring-1 focus:ring-white rounded-md p-1 w-85 font-[Inter]"
+              placeholder="Digite a descrição da tarefa"
+            />
+            <input
+              type="submit"
+              value="Adicionar"
+              className="border rounded-lg cursor-pointer w-85 bg-white font-bold font-[Inter] text-[14px] p-1 border-none shadow-md"
+            />
+          </form>
         </div>
         <div className="mt-4">
-          <Tasks />
+          <Tasks tasks={tasks} />
         </div>
       </div>
     </>
--- to-do-list/src/components/Tasks.tsx
+++ to-do-list/src/components/Tasks.tsx
@@ -1,9 +1,44 @@
-export default function Tasks() {
+import type { Task } from "../types/types.ts";
+
+interface TasksProps {
+  tasks: Task[];
+}
+
+const Tasks: React.FC<TasksProps> = ({ tasks }) => {
+  const tasksCompleted = tasks.filter((tasks) => tasks.isCompleted);
+
   return (
     <div className="flex flex-col gap-4 p-4 rounded-tl-4xl shadow-md bg-white min-h-screen">
-      <h2 className="inline-flex text-left font-bold relative pb-2 w-auto">TAREFAS
-        <span className='absolute bottom-0 left-0 w-17 h-1 bg-purple-700 rounded-lg'></span>
+      <h2 className="inline-flex text-left font-bold relative pb-2 w-auto font-[Inter]">
+        TAREFAS
+        <span className="absolute bottom-2 left-0 w-18 h-0.5 bg-purple-700 rounded-lg"></span>
       </h2>
+      <div className="flex gap-2">
+        <span className="border border-gray-500 rounded-md p-2 shadow-md font-[inter]">
+          {tasksCompleted.length}
+          {tasksCompleted.length > 1 || tasksCompleted.length == 0
+            ? " Completas 🏆"
+            : " Completa 🏆"}
+        </span>
+        <span className="border border-gray-500 rounded-md p-2 shadow-md font-[inter]">
+          {tasks.length - tasksCompleted.length}
+          {tasks.length > 1 || tasks.length == 0
+            ? " Pendentes 📅"
+            : " Pendente 📅"}
+        </span>
+      </div>
+      <ul className="flex flex-col gap-2">
+        {tasks.map((task) => (
+          <li
+            key={task.id}
+            className="flex bg-[#313d5a] rounded-sm p-2 h-10 items-center font-bold text-[14px] font-[Inter] shadow-sm text-white"
+          >
+            {task.title}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}   
+};
+
+export default Tasks;
